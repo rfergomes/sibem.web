55d5b46fceda8b7879b334dbbd4470dd31ab4b23
Rodrigo Fernando Gomes Lima <rfergomes@gmail.com>
2026-05-28 14:23:44 -0300

    feat: add custom 404 error page template

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,7 +10,7 @@
     
     @include('partials.pwa-meta')
     
-    <link rel="icon" href="{{ asset('assets/images/favicon.svg') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('assets/images/favicon.svg') }}?v=2" type="image/svg+xml">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" id="main-style-link">
 </head>
