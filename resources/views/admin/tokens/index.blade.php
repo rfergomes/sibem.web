0d2b3c8677157953411a620a17b9c4c67d5d358b
Rodrigo Fernando Gomes Lima <rfergomes@gmail.com>
2026-05-29 11:09:22 -0300

    feat: create index view for desktop token management with generation and revocation capabilities

--- resources/views/admin/tokens/index.blade.php
+++ resources/views/admin/tokens/index.blade.php
@@ -9,7 +9,7 @@
         <div class="card">
             <div class="card-header bg-dark d-flex align-items-center justify-content-between">
                 <h4 class="mb-0 text-white"><i class="ti ti-list me-2"></i>Tokens Desktop Ativos</h4>
-                <button type="button" class="btn btn-light-primary" data-bs-toggle="modal" data-bs-target="#gerarTokenModal">
+                <button type="button" class="btn btn-light-success" data-bs-toggle="modal" data-bs-target="#gerarTokenModal">
                     <i class="ti ti-plus me-1"></i> Gerar Novo Token
                 </button>
             </div>
